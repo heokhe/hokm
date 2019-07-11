51d9a7abb6d2aa2342ca2452cb4ef9da082e3806
Hosein Khansari <hrk.x11@gmail.com>
2019-07-11 11:02:05 +0430

    Add clickable prop to <Card>, make cards smaller on mobile

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,11 +1,11 @@
 <script>
 import Icon from './Icon.svelte';
 import { stringifyNumber as stringify } from '../core/utils/';
-export let type, number = 0;
+export let type, number = 0, clickable = false;
 $: isRed = type === 'D' || type === 'K';
 </script>
 
-<div class="card {isRed ? 'red' : ''} {!number ? 'suite-only' : ''}" on:click>
+<div class="card {isRed ? 'red' : ''} {!number ? 'suite-only' : ''} {clickable ? 'clickable' : ''}" on:click>
   <Icon size='1.1em' {type} />
   {#if number}
     <span>{stringify(number)}</span>
