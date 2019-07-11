a962ea7d27f616d65abb90181b4f0f4191e51f82
Hosein Khansari <hrk.x11@gmail.com>
2019-07-11 14:10:27 +0430

    Use new components and styles in Game

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,24 +1,29 @@
 <script>
-import Card from './components/Card.svelte';
-import GameHeader from './components/GameHeader.svelte';
-import CardIcon from './components/CardIcon.svelte';
 import { onMount } from 'svelte';
 import { CARD_TYPES } from './core/card';
 import { game, isPlaying } from './store';
+import Card from './components/Card.svelte';
+import Move from './components/Move.svelte';
+import GameHeader from './components/GameHeader.svelte';
+import Board from './components/Board.svelte';
+
 $: mustSelectTrumpSuite = !$game.trumpSuite;
 let activeCards, base, cards = [], tricks;
+
 onMount(() => {
   activeCards = [];
   base = null;
   cards = $game.me.availableCards;
-  tricks = [0,0];
+  tricks = [0, 0];
 })
+
 $game.on('move', () => {
   cards = $game.me.availableCards;
   activeCards = $game.activeCards;
   base = $game.baseSuite;
   tricks = $game.tricks;
 })
+
 $game.on('end', ({ winner: { members: [a, b] } }) => {
   alert(`the game has ended. ${a.name} and ${b.name} won!`);
   $isPlaying = false
@@ -29,27 +34,24 @@ $game.on('end', ({ winner: { members: [a, b] } }) => {
   {#if !mustSelectTrumpSuite}
     <GameHeader {tricks} baseSuite={base} trumpSuite={$game.trumpSuite} />
   {/if}
-  <main class="game-board {mustSelectTrumpSuite ? 'centered' : ''}">
+  <Board>
     {#if mustSelectTrumpSuite}
-      <h1 class="text-2xl font-medium">Select a trump suite</h1>
-      <div>
+      <h1 class="text-3xl tracking-wide font-light text-gray-800 text-center">Select a trump suite</h1>
+      <div class="mt-3">
         {#each CARD_TYPES as type}
-          <Card {type} on:click={() => $game.trumpSuite = type} />
+          <Card {type} clickable on:click={() => $game.trumpSuite = type} />
         {/each}
       </div>
     {:else}
-      {#each activeCards as { number, type, owner: { name } } }
-        <div>
-          player: {name}
-          <Card {number} {type} />
-        </div>
+      {#each activeCards as { number, type, owner }}
+        <Move card={{ number, type }} bot={owner.name !== 'me'} position={$game.players.indexOf(owner)} />
       {/each}
     {/if}
-  </main>
+  </Board>
   <section class="cards">
     {#each cards as card, i}
-      {#if !mustSelectTrumpSuite || i < 5}
-        <Card number={card.number} type={card.type} on:click={() => $game.me.move(card)} />
+      {#if !mustSelectTrumpSuite || 5 > i}
+        <Card number={card.number} type={card.type} on:click={() => $game.me.move(card)} clickable />
       {/if}
     {/each}
   </section>
