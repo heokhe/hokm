c883535c9f346f374a4aee1a86b338867e7fe0ef
Hosein Khansari <hrk.x11@gmail.com>
2019-07-11 11:29:42 +0430

    Create <Move> & <Board> components

---
+++ src/components/Board.svelte
@@ -0,0 +1,7 @@
+<script>
+let height = 0;
+</script>
+
+<main class="game-board p-10" bind:clientHeight={height} style="width: {height}px">
+  <slot />
+</main>
---
+++ src/components/Move.svelte
@@ -0,0 +1,13 @@
+<script>
+import Card from './Card.svelte';
+import Icon from './Icon.svelte';
+export let bot, card, position;
+$: style = `${['bottom', 'right', 'top', 'left'][position]}: 1.5rem`;
+</script>
+
+<div class="move {bot ? 'is-bot' : ''}" {style}>
+  <div class="avatar">
+    <Icon type={bot ? 'bot' : ''} size={24} />
+  </div>
+  <Card type={card.type} number={card.number} />
+</div>
