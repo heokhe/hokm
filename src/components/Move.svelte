d8d2db7f31c295be39efe3c00f9e16bca2de4342
Hosein Khansari <hrk.x11@gmail.com>
2019-07-16 16:00:19 +0430

    Properly align <Move> in different positions

--- src/components/Move.svelte
+++ src/components/Move.svelte
@@ -2,12 +2,14 @@
 import Card from './Card.svelte';
 import Icon from './Icon.svelte';
 export let bot, card, position;
-$: style = `${['bottom', 'right', 'top', 'left'][position]}: 1.5rem`;
+$: positionName = ['bottom', 'right', 'top', 'left'][position];
 </script>
 
-<div class="move {bot ? 'is-bot' : ''}" {style}>
+<div class="move {bot ? 'is-bot' : ''} position-{positionName}">
   <div class="avatar">
     <Icon type={bot ? 'bot' : ''} size={24} />
   </div>
-  <Card type={card.type} number={card.number} />
+  {#if card}
+    <Card type={card.type} number={card.number} />
+  {/if}
 </div>
