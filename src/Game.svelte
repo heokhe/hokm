90dde67691e60ec13c96fed44ae6af5597ca0ada
Hosein Khansari <hrk.x11@gmail.com>
2019-07-16 16:20:50 +0430

    Sort cards in <Game>, Make <Move>s persistent, Use <TrumpSelect>

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,10 +1,12 @@
 <script>
 import { onMount } from 'svelte';
 import { CARD_TYPES } from './core/card';
+import { sortCards } from './core/utils';
 import { game } from './store';
 import Card from './components/Card.svelte';
 import Move from './components/Move.svelte';
 import GameHeader from './components/GameHeader.svelte';
+import TrumpSelect from './components/TrumpSelect.svelte';
 import Board from './components/Board.svelte';
 
 let activeCards = [], base = null, cards = [], tricks = [0, 0], mustSelectTrumpSuite = true;
@@ -41,25 +43,18 @@ $game.on('end', ({ winner: { members: [a, b] } }) => {
   {#if !mustSelectTrumpSuite}
     <GameHeader {tricks} baseSuite={base} trumpSuite={$game.trumpSuite} />
   {/if}
-  <Board>
+  <Board squared={!mustSelectTrumpSuite}>
     {#if mustSelectTrumpSuite}
-      <h1 class="text-3xl tracking-wide font-light text-gray-800 text-center">Select a trump suite</h1>
-      <div class="mt-3">
-        {#each CARD_TYPES as type}
-          <Card {type} clickable on:click={() => init(type)} />
-        {/each}
-      </div>
+      <TrumpSelect on:select={init} />
     {:else}
-      {#each activeCards as { number, type, owner }}
-        <Move card={{ number, type }} bot={owner.name !== 'me'} position={$game.players.indexOf(owner)} />
+      {#each $game.players as player, i}
+        <Move card={activeCards.find(c => c.owner === player)} bot={player.name !== 'me'} position={i} />
       {/each}
     {/if}
   </Board>
   <section class="cards">
-    {#each cards as card, i}
-      {#if !mustSelectTrumpSuite || 5 > i}
-        <Card number={card.number} type={card.type} on:click={() => $game.me.move(card)} clickable={!mustSelectTrumpSuite} />
-      {/if}
+    {#each sortCards(cards.slice(0, mustSelectTrumpSuite ? 5 : 13)) as card}
+      <Card number={card.number} type={card.type} on:click={() => $game.me.move(card)} clickable={!mustSelectTrumpSuite} />
     {/each}
   </section>
 </div>
