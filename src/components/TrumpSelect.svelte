26a70961bd16e56638094d109040dbd1810eebb6
Hosein Khansari <hrk.x11@gmail.com>
2019-07-16 15:40:50 +0430

    Create <TrumpSelect>

---
+++ src/components/TrumpSelect.svelte
@@ -0,0 +1,13 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+import Card from './Card.svelte';
+import { CARD_TYPES as types } from '../core/card';
+const dispatch = createEventDispatcher();
+</script>
+
+<h1 class="text-3xl tracking-wide font-light text-gray-800 text-center">Select a trump suite</h1>
+<div class="mt-3">
+  {#each types as type}
+    <Card {type} clickable on:click={() => dispatch('select', type)} />
+  {/each}
+</div>
