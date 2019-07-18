5c0a262e9bec96aee22997d1b93a36d60caf5534
Hosein Khansari <hrk.x11@gmail.com>
2019-07-18 17:36:15 +0430

    Render nothing when no icon is found for `type`

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,6 +1,5 @@
 <script>
-export let type;
-export let size = 24;
+export let type, size = 25;
 $: d
   = type === 'D' ? 'M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z'
   : type === 'K' ? 'M19,12L12,22L5,12L12,2Z'
@@ -10,6 +9,8 @@ $: d
   : ''
 </script>
 
-<svg viewBox='0 0 24 24' width={size} height={size}>
-  <path fill='currentColor' {d}></path>
-</svg>
+{#if d !== ''}
+  <svg viewBox="0 0 24 24" width={size} height={size}>
+    <path class="fill-current" {d}></path>
+  </svg>
+{/if}
